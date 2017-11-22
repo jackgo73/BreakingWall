a6bd5dfcba6c7f9388fec3e7dbf3e75d102d5301
Jack Go <mutex73@gmail.com>
2017-11-22 15:31:49 +0800

    basic framework done

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -1,9 +1,29 @@
 
-// Initializations
+
 
 'use strict';
 
+
+let popup = {};
+
+// Initializations
+
 document.addEventListener('DOMContentLoaded', function () {
     let version;
+    version = chrome.runtime.getManifest().version;
+    document.getElementById('version-label').innerText = version;
 
-}
+    let websiteContextElement, protectionToggleElement, domainIndicatorElement;
+
+    websiteContextElement = document.getElementById('website-context');
+    protectionToggleElement = document.getElementById('protection-toggle-button');
+    domainIndicatorElement = document.getElementById('domain-indicator');
+
+    domainIndicatorElement.innerText = 'baidu.com';
+    websiteContextElement.setAttribute('class', 'panel');
+    protectionToggleElement.setAttribute('class', 'button button-toggle active');
+
+
+});
+
+
