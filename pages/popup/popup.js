cd9c0fcaebff0df1720411df12452a9ff8c861c0
Jack Go <mutex73@gmail.com>
2017-12-06 16:23:39 +0800

    add proxy code

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -21,86 +21,103 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('version-label').innerText = version;
 
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.storage.local.get('amountProxy', function (items) {
 
-        chrome.runtime.getBackgroundPage(function (backgroundPage) {
+        let amountProxy = items.amountProxy || 0;
+        document.getElementById('proxy-counter').innerText = amountProxy;
 
-            if (backgroundPage === null) {
-                return;
-            }
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
 
-            popup.backgroundPage = backgroundPage;
+            chrome.runtime.getBackgroundPage(function (backgroundPage) {
 
-
-            let domain;
-            try {
-                domain = tabs[0].url.match(WEB_DOMAIN_EXPRESSION)[1];
-            } catch (exception) {
-                domain = null;
-            }
-            if (domain !== null) {
-
-                let websiteContextElement, proxyToggleElement, domainIndicatorElement;
-
-                websiteContextElement = document.getElementById('website-context');
-                proxyToggleElement = document.getElementById('proxy-toggle-button');
-                domainIndicatorElement = document.getElementById('domain-indicator');
-
-                if (domain.startsWith(WEB_PREFIX_VALUE)) {
-                    domain = domain.slice(WEB_PREFIX_LENGTH);
+                if (backgroundPage === null) {
+                    return;
                 }
 
-                domainIndicatorElement.innerText = domain;
+                popup.backgroundPage = backgroundPage;
 
-                if (!backgroundPage.requestAnalyzer.whitelistedDomains[domain] ) {
-                    /**
-                     * no need to proxy!
-                     */
-                    proxyToggleElement.setAttribute('class', 'button button-toggle active');
 
-                    let disableProxyTitle = chrome.i18n.getMessage('disableProxyTitle');
-                    proxyToggleElement.setAttribute('title', disableProxyTitle);
+                let domain;
+                try {
+                    domain = tabs[0].url.match(WEB_DOMAIN_EXPRESSION)[1];
+                } catch (exception) {
+                    domain = null;
+                }
+                if (domain !== null) {
 
-                    proxyToggleElement.addEventListener('click', function () {
+                    let websiteContextElement, proxyToggleElement, domainIndicatorElement;
+
+                    websiteContextElement = document.getElementById('website-context');
+                    proxyToggleElement = document.getElementById('proxy-toggle-button');
+                    domainIndicatorElement = document.getElementById('domain-indicator');
+
+                    if (domain.startsWith(WEB_PREFIX_VALUE)) {
+                        domain = domain.slice(WEB_PREFIX_LENGTH);
+                    }
+
+                    domainIndicatorElement.innerText = domain;
+
+                    if (!backgroundPage.requestAnalyzer.whitelistedDomains[domain]) {
                         /**
-                         * click means the url need to proxy and add to the white list
+                         * no need to proxy!
                          */
-                        backgroundPage.stateManager.addDomainToWhitelist(domain).then(function () {
+                        proxyToggleElement.setAttribute('class', 'button button-toggle');
 
-                        chrome.tabs.reload(tabs[0].id, {
-                            'bypassCache': true
+                        let enableProxyTitle = chrome.i18n.getMessage('enableProxyTitle');
+                        proxyToggleElement.setAttribute('title', enableProxyTitle);
+
+                        proxyToggleElement.addEventListener('click', function () {
+                            /**
+                             * click means the url need to proxy and add to the white list
+                             */
+                            backgroundPage.stateManager.addDomainToWhitelist(domain).then(function () {
+
+                                chrome.tabs.reload(tabs[0].id, {
+                                    'bypassCache': true
+                                });
+
+                                return window.close();
+                            });
                         });
 
-                        return window.close();
-                        });
-                    });
+                        // chrome.proxy.settings.set(conf, function (){
+                        //     //alert('set proxy ok');
+                        // });
 
-                } else {
-                    /**
-                     * need to proxy!
-                     */
-                    proxyToggleElement.setAttribute('class', 'button button-toggle');
-                    let enableProxyTitle = chrome.i18n.getMessage('enableProxyTitle');
-                    proxyToggleElement.setAttribute('title', enableProxyTitle);
-
-                    proxyToggleElement.addEventListener('click', function () {
+                    } else {
                         /**
-                         * click means the url do not need to proxy anymore
+                         * need to proxy!
                          */
-                        backgroundPage.stateManager.deleteDomainFromWhitelist(domain).then(function () {
+                        proxyToggleElement.setAttribute('class', 'button button-toggle active');
+                        let disableProxyTitle = chrome.i18n.getMessage('disableProxyTitle');
+                        proxyToggleElement.setAttribute('title', disableProxyTitle);
 
-                        chrome.tabs.reload(tabs[0].id, {
-                            'bypassCache': true
+                        proxyToggleElement.addEventListener('click', function () {
+                            /**
+                             * click means the url do not need to proxy anymore
+                             */
+                            backgroundPage.stateManager.deleteDomainFromWhitelist(domain).then(function () {
+
+                                chrome.tabs.reload(tabs[0].id, {
+                                    'bypassCache': true
+                                });
+
+                                return window.close();
+                            });
                         });
 
-                        return window.close();
+                        chrome.proxy.settings.set(conf, function (){
+                            //alert('set proxy ok');
                         });
-                    });
+                    }
+
+
+
+                    websiteContextElement.setAttribute('class', 'panel');
                 }
 
-                websiteContextElement.setAttribute('class', 'panel');
-            }
 
+            });
 
         });
 
